41c20f8d6f2e9c133db5e90fb8190832efaf6d98
Mehrshad Sahebsara <msahebsara@live.ca>
2021-09-10 10:11:25 -0600

    Modify py script to download itself from gh

--- script.py
+++ script.py
@@ -2,4 +2,6 @@ import requests
 
 print(requests.__version__)
 
-print(requests.get("http://www.google.com/").content)
+print(requests.get("http://www.google.com/").text)
+
+print(requests.get("https://raw.githubusercontent.com/msahebsara/c404lab1/master/script.py").text)
